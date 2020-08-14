fe6c2639d428c210b3f0cf377d1f2cd971c063d4
iida.nao08@gmail.com <genome@NCCT20190221C.local>
2020-08-14 16:59:29 +0900

    modify_for_pipeline

--- juncmut/juncmut_juncutils.py
+++ juncmut/juncmut_juncutils.py
@@ -9,13 +9,11 @@ Created on Wed Jul 31 2019
 def juncmut_juncutils(input_file, output_file, cont_list, genome_id, read_num_thres):
     import subprocess
     import shutil
-    import pandas as pd
     import os
-    import glob
     from junc_utils.utils import proc_star_junction
     #autochom
 
-    infile = input_file
+
     tmpfile_list = []
     tmpfile1 = output_file + ".tmp1"
     tmpfile_list.append(tmpfile1)
@@ -29,20 +27,6 @@ def juncmut_juncutils(input_file, output_file, cont_list, genome_id, read_num_th
 
                 print('\t'.join(F), file = hout)
 
-    # indf1 = pd.read_csv(infile, sep='\t', header=None, dtype=str)
-    # indf2 = indf1[indf1.iloc[:,0] in [target_rnames] + ["chr" + x for x in target_rnames]]
-    # indf2.to_csv(tmpfile1, sep='\t', header=False, index=False)
-    """
-    ##########
-    # is this necessary? (YS)   
-    if rbamchr == 'chr':
-        indf2 = indf1[(indf1.iloc[:,0]=='chr1')|(indf1.iloc[:,0]=='chr2')|(indf1.iloc[:,0]=='chr3')|(indf1.iloc[:,0]=='chr4')|(indf1.iloc[:,0]=='chr5')|(indf1.iloc[:,0]=='chr6')|(indf1.iloc[:,0]=='chr7')|(indf1.iloc[:,0]=='chr8')|(indf1.iloc[:,0]=='chr9')|(indf1.iloc[:,0]=='chr10')|(indf1.iloc[:,0]=='chr11')|(indf1.iloc[:,0]=='chr12')|(indf1.iloc[:,0]=='chr13')|(indf1.iloc[:,0]=='chr14')|(indf1.iloc[:,0]=='chr15')|(indf1.iloc[:,0]=='chr16')|(indf1.iloc[:,0]=='chr17')|(indf1.iloc[:,0]=='chr18')|(indf1.iloc[:,0]=='chr19')|(indf1.iloc[:,0]=='chr20')|(indf1.iloc[:,0]=='chr21')|(indf1.iloc[:,0]=='chr22')|(indf1.iloc[:,0]=='chrX')]
-        indf2.to_csv(tmpfile1, sep='\t', header=False, index=False)
-    else:
-        indf2 = indf1[(indf1.iloc[:,0]=='1')|(indf1.iloc[:,0]=='2')|(indf1.iloc[:,0]=='3')|(indf1.iloc[:,0]=='4')|(indf1.iloc[:,0]=='5')|(indf1.iloc[:,0]=='6')|(indf1.iloc[:,0]=='7')|(indf1.iloc[:,0]=='8')|(indf1.iloc[:,0]=='9')|(indf1.iloc[:,0]=='10')|(indf1.iloc[:,0]=='11')|(indf1.iloc[:,0]=='12')|(indf1.iloc[:,0]=='13')|(indf1.iloc[:,0]=='14')|(indf1.iloc[:,0]=='15')|(indf1.iloc[:,0]=='16')|(indf1.iloc[:,0]=='17')|(indf1.iloc[:,0]=='18')|(indf1.iloc[:,0]=='19')|(indf1.iloc[:,0]=='20')|(indf1.iloc[:,0]=='21')|(indf1.iloc[:,0]=='22')|(indf1.iloc[:,0]=='X')]
-        indf2.to_csv(tmpfile1, sep='\t', header=False, index=False)
-    """
-    #########
 
     tmpfile2 = output_file + ".tmp2"
     
@@ -60,8 +44,9 @@ def juncmut_juncutils(input_file, output_file, cont_list, genome_id, read_num_th
 
         shutil.copy(cur_infile, tmpfile2)
         tmpfile_list.append(tmpfile2)
-        
-    annotate_commands = ["junc_utils", "annotate", tmpfile2, output_file, "--genome_id", genome_id, '--gene_model=gencode']
+    
+    output_path = output_file
+    annotate_commands = ["junc_utils", "annotate", tmpfile2, output_path, "--genome_id", genome_id, '--gene_model=gencode']
     subprocess.call(annotate_commands)
 
     for tfile in tmpfile_list:
@@ -73,10 +58,10 @@ if __name__== "__main__":
 
     parser = argparse.ArgumentParser() #make a parser
     
-    parser.add_argument("input", metavar = "prefix", default = None, type = str,
+    parser.add_argument("input_file", metavar = "input_file", default = None, type = str,
                             help = "prefix") 
         
-    parser.add_argument("folder", metavar = "group", default = "my_samples", type = str,
+    parser.add_argument("output_file", metavar = "output_file", default = "my_sample", type = str,
                             help = "folder name of input files") 
         
     parser.add_argument('--control_file', nargs='*', type = str, default = [],
@@ -85,18 +70,18 @@ if __name__== "__main__":
     parser.add_argument("--genome_id", choices = ["hg19", "hg38", "mm10"], default = "hg19",
                               help = "Genome id used for selecting UCSC-GRC chromosome name corresponding files (default: %(default)s)")
     
-    parser.add_argument("--rbam_chr_prefix", choices = ["chr", "none"], default = "none",
+    parser.add_argument("--read_num_thres", metavar = "read_num_thres", default = 1, type = int,
                               help = "chr prefix used in your bam (default: %(default)s)")
     
     args = parser.parse_args()
 
-    pr = args.input
-    folder = args.folder
+    input_file = args.input_file
+    output_file = args.output_file
     cont_list = args.control_file
     genome_id = args.genome_id
-    rbamchr = args.rbam_chr_prefix
-    print(pr) 
-    juncmut_juncutils(pr, folder, cont_list, genome_id, rbamchr)
+    read_num_thres = args.read_num_thres
+    print(input_file)
     
+    juncmut_juncutils(input_file, output_file, cont_list, genome_id, read_num_thres)
 
 
