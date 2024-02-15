33e7403c26ba09c68d7fa6fec910d797fd0dbf55
aokad <aokada@ncc.go.jp>
2024-02-15 19:16:48 +0900

    modify output fileds

--- juncmut/juncmut_assadj.py
+++ juncmut/juncmut_assadj.py
@@ -7,7 +7,7 @@ def juncmut_assadj(input_file, output_file):
     with open(input_file) as hin, open(output_file, 'w') as hout:
         # [TODO] set sample
         csvwriter = csv.DictWriter(hout, delimiter='\t', lineterminator='\n', fieldnames=[
-            "Chr", "Start_ori", "End_ori", "SJ_2", "SJ_3", "Sample", "Splicing_class", "SJ_strand", "SJ_read_count", 
+            "Chr", "SJ_2", "SJ_3", "Start_ori", "End_ori", "Sample", "Splicing_class", "SJ_strand", "SJ_read_count", 
         ])
         csvwriter.writeheader()
         
@@ -18,8 +18,8 @@ def juncmut_assadj(input_file, output_file):
             
             out_csvobj = {
                 "Chr": csvobj["SJ_1"],
-                "SJ_2": csvobj["SJ_2"],
-                "SJ_3": csvobj["SJ_3"],
+                "Start_ori": csvobj["SJ_2"],
+                "End_ori": csvobj["SJ_3"],
                 "Sample": "",
                 "Splicing_class": csvobj["Splicing_Class"],
                 "SJ_read_count": csvobj["SJ_7"],
@@ -41,8 +41,8 @@ def juncmut_assadj(input_file, output_file):
             elif "s" in csvobj["Is_Boundary_1"] or "e" in csvobj["Is_Boundary_2"]:
                 strand = "-"
             
-            out_csvobj["Start_ori"] = int(csvobj["SJ_2"]) - 1 * int(offset)
-            out_csvobj["End_ori"] = int(csvobj["SJ_3"]) - 1 * int(offset)
+            out_csvobj["SJ_2"] = int(csvobj["SJ_2"]) - 1 * int(offset)
+            out_csvobj["SJ_3"] = int(csvobj["SJ_3"]) - 1 * int(offset)
             out_csvobj["SJ_strand"] = strand
             csvwriter.writerow(out_csvobj)
 
