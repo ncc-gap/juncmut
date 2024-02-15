1ecb733fccc469c891b97f9b9dc978955eebcbec
aokad <aokada@ncc.go.jp>
2024-02-15 16:04:54 +0900

    modify create keys

--- juncmut/juncmut_freq.py
+++ juncmut/juncmut_freq.py
@@ -8,22 +8,22 @@ def juncmut_freq(input_file, output_file, original_sj_file, read_num_thres, freq
     with open(input_file) as hin:
         csvreader = csv.DictReader(hin, delimiter='\t')
         for csvobj in csvreader:
-            key1 = "%s,%s,%s" % (csvobj["Chr"], csvobj["Start_ori"], csvobj["End_ori"])
-            key2 = "%s,%s" % (csvobj["Splicing_class"], csvobj["SJ_strand"])
-            if not key1 in junc_obj:
-                junc_obj[key1] = {
+            sj_pos = "%s,%s,%s" % (csvobj["Chr"], csvobj["SJ_2"], csvobj["SJ_3"])
+            splicing_type = "%s,%s" % (csvobj["Splicing_class"], csvobj["SJ_strand"])
+            if not sj_pos in junc_obj:
+                junc_obj[sj_pos] = {
                     "SJ_read_count": 0
                 }
-            if not key2 in junc_obj[key1]:
-                junc_obj[key1][key2] = {
-                    "SJ_2": [],
-                    "SJ_3": [],
+            if not splicing_type in junc_obj[sj_pos]:
+                junc_obj[sj_pos][splicing_type] = {
+                    "Start_ori": [],
+                    "End_ori": [],
                     "Sample": [],
                 }
-            junc_obj[key1][key2]["SJ_2"].append(csvobj["SJ_2"])
-            junc_obj[key1][key2]["SJ_3"].append(csvobj["SJ_3"])
-            junc_obj[key1][key2]["Sample"].append(csvobj["Sample"])
-            junc_obj[key1]["SJ_read_count"] += int(csvobj["SJ_read_count"])
+            junc_obj[sj_pos][splicing_type]["Start_ori"].extend(csvobj["Start_ori"].split(";"))
+            junc_obj[sj_pos][splicing_type]["End_ori"].extend(csvobj["End_ori"].split(";"))
+            junc_obj[sj_pos][splicing_type]["Sample"].append(csvobj["Sample"])
+            junc_obj[sj_pos]["SJ_read_count"] += int(csvobj["SJ_read_count"])
     
     original_sj_starts = {}
     original_sj_ends = {}
@@ -34,47 +34,47 @@ def juncmut_freq(input_file, output_file, original_sj_file, read_num_thres, freq
             start = F[1]
             end = F[2]
             read = int(F[6])
-            key1 = "%s_%s" % (chrom, start)
-            if not key1 in original_sj_starts:
-                original_sj_starts[key1] = read
+            start_pos = "%s_%s" % (chrom, start)
+            if not start_pos in original_sj_starts:
+                original_sj_starts[start_pos] = read
             else:
-                original_sj_starts[key1] += read
+                original_sj_starts[start_pos] += read
             
-            key2 = "%s_%s" % (chrom, end)
-            if not key2 in original_sj_ends:
-                original_sj_ends[key2] = read
+            end_pos = "%s_%s" % (chrom, end)
+            if not end_pos in original_sj_ends:
+                original_sj_ends[end_pos] = read
             else:
-                original_sj_ends[key2] += read
-    
+                original_sj_ends[end_pos] += read
+
     with open(output_file, 'w') as hout:
         csvwriter = csv.DictWriter(hout, delimiter='\t', lineterminator='\n', fieldnames=[
             "Chr", "Start", "End", "Start_list", "End_list", "Sample", "Splicing_class", "SJ_strand", "SJ_read_count", "SJ_depth", "SJ_freq", 
         ])
         csvwriter.writeheader()
 
-        for key1 in sorted(list(junc_obj.keys())):
+        for sj_pos in sorted(list(junc_obj.keys())):
             # [TODO] fix this case
-            if len(junc_obj[key1]) > 2:
-                raise Exception("juncmut_freq.py: Unexpected data %s key:%s" % (input_file, key1))
+            if len(junc_obj[sj_pos]) > 2:
+                raise Exception("juncmut_freq.py: Unexpected data %s key:%s" % (input_file, sj_pos))
 
-            (chr, start_ori, end_ori) = key1.split(",")
-            sj_read_count = junc_obj[key1]["SJ_read_count"]
+            (chr, sj_2, sj_3) = sj_pos.split(",")
+            sj_read_count = junc_obj[sj_pos]["SJ_read_count"]
 
-            for key2 in sorted(list(junc_obj[key1].keys())):
-                if key2 == "SJ_read_count": continue
+            for key in sorted(list(junc_obj[sj_pos].keys())):
+                if key == 'SJ_read_count': continue
+                splicing_type = key
+                (splicing_class, sj_strand) = splicing_type.split(",")
 
-                (splicing_class, sj_strand) = key2.split(",")
-
-                start_ls = sorted(list(set(junc_obj[key1][key2]["SJ_2"]+[start_ori])))
-                end_ls = sorted(list(set(junc_obj[key1][key2]["SJ_3"]+[end_ori])))
+                start_ls = sorted(list(set(junc_obj[sj_pos][splicing_type]["Start_ori"]+[sj_2])))
+                end_ls = sorted(list(set(junc_obj[sj_pos][splicing_type]["End_ori"]+[sj_3])))
 
                 out_csvobj = {
                     "Chr": chr,
-                    "Start": start_ori,
-                    "End": end_ori,
+                    "Start": sj_2,
+                    "End": sj_3,
                     "Start_list": ";".join(start_ls),
                     "End_list": ";".join(end_ls),
-                    "Sample": ";".join(junc_obj[key1][key2]["Sample"]),
+                    "Sample": "",
                     "Splicing_class": splicing_class,
                     "SJ_strand": sj_strand,
                     "SJ_read_count": sj_read_count,
